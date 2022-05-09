9acb00c34d5a17d08a0a7b4ed1e4d69653b60c4a
LuoXianming <2662256597@qq.com>
2022-05-09 21:26:17 +0800

    week11 commit

---
+++ src/main/java/com/luoxianming/week11/homework/Dog.java
@@ -0,0 +1,18 @@
+package com.luoxianming.week11.homework;
+
+public class Dog {
+
+    private String name;
+
+    public Dog(){
+
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+}
